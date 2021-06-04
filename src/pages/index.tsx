57ef7e2c9187e0ed2ad3c3cda1a9e651580434f0
Mateus <mateushbsb@gmail.com>
2021-06-04 20:43:03 -0300

    header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,11 @@
 import  Head from 'next/head';
 
-import styles from '../styles/home.module.scss';
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1 className={styles.title}>Hello</h1>
     </>
   )
 }
