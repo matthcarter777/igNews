98805c9b3db4a100ad23d8b0307c4724dabb2a83
Mateus <mateushbsb@gmail.com>
2021-06-14 21:41:57 -0300

    payment

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,33 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from 'next-auth/client';
+import { stripe } from '../../services/stripe';
+
+export default async(req: NextApiRequest, res: NextApiResponse) => {
+  if (req.method === 'POST') {
+    const session = await getSession({ req });
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+      // metadata
+    })
+    
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [{
+        price: 'price_1Iz56HIi2RRGNZtBPcnI2M2J',
+        quantity: 1
+      }],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: 'http://localhost:3000/posts',
+      cancel_url: 'http://localhost:3000/'
+    })
+
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method not allowed');
+  }
+}
